2de92ce7bab9838eb20901b884c2b7e643e8ccaf
Sally <Sally@DESKTOP-I8K5FF8>
2024-11-26 22:16:16 +0200

    Add project files.

---
+++ LeetCode/VariableAfterOperations.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode
+{
+    internal class VariableAfterOperations
+    {
+        public static void Run()
+        {
+            string[] operations = ["--X", "X++", "X++"];
+            int result = FinalValueAfterOperations(operations);
+            Console.WriteLine(result);
+        }
+
+        public static int FinalValueAfterOperations(string[] operations)
+        {
+
+            int sum= 0;
+
+            foreach (string op in operations) 
+            {
+                if (op == "++X" || op == "X++")
+                {
+                    sum += 1;
+                }
+                else if (op == "X--" || op == "--X")
+                {
+                    sum -= 1;
+
+                }
+            }
+            return sum;
+        }
+    }
+}
